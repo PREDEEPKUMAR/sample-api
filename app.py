4889c7e75962d39fc740ca2558f80bd19657ee79
Predi <50257387+PREDEEPKUMAR@users.noreply.github.com>
2023-07-08 13:49:46 +0000

    add api

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import requests
 
 print('Hello World')
+
+
+response = requests.get('https://predeepkumar-organic-fiesta-qp5xw75jrpq2x44w-8000.preview.app.github.dev/details')
+data = response.json()
+print(data)
